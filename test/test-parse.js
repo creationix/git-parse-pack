4f60238bfb8164ca75fac6dc8dd7adef96ef2f85
Tim Caswell <tim@creationix.com>
2013-07-04 17:56:15 -0500

    Convert test into automated test

--- test/test-parse.js
+++ test/test-parse.js
@@ -1,10 +1,35 @@
 var parse = require('../.');
+var bops = require('bops');
 
+var i = 0;
+var version;
+var num;
+var count = 0;
+var done;
 var emit = parse(function (err, item) {
   if (err) throw err;
-  console.log(item);
+  if (i++ === 0) {
+    version = item.version;
+    num = item.num;
+  }
+  else if (item === undefined) {
+    done = true;
+  }
+  else if (!bops.is(item)) {
+    count++;
+  }
 });
 
 var sample = require('fs').readFileSync(__dirname + "/sample.pack");
 emit(null, sample);
 emit();
+
+console.log({
+  version: version,
+  num: num,
+  count: count,
+  done: done
+});
+
+if (num !== count) throw new Error("Count mismatch " + num + " != " + count);
+if (!done) throw new Error("Missing stream end");
