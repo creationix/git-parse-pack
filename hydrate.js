6505906c3f601b663de818f565e500480f99f4d3
Tim Caswell <tim@creationix.com>
2013-07-10 17:21:47 -0500

    Partially refactor hydrate to new interface

--- hydrate.js
+++ hydrate.js
@@ -1,151 +1,267 @@
-var sha1 = require('sha1-digest');
 var subStream = require('sub-stream');
+var each = require('simple-stream-helpers/each.js');
+var bops = require('bops');
+var arraySource = require('simple-stream-helpers/array-source.js');
 var pushToPull = require('push-to-pull');
 var applyDelta = require('./apply-delta.js');
-var bops = require('bops');
-var types = {
-  "1": "commit",
-  "2": "tree",
-  "3": "blob",
-  "4": "tag"
-};
 
-// A pull-filter that accepts a flat stream of objects and bodies
-// Also accepts an implementation of find(item, callback(err, item, target) {} )
-// find accepts an item with nested body stream and returns (in callback) same
-// item and what it references.
-// Output is a new nested stream of all objects with deltas applied.
-// The items will initially have null for their hash property.
-// But after consuming the stream, hash will be set properly.
-// (stream<header|body>, temp) -> stream<header_with_body>
-module.exports = function (stream, find) {
+// Accepts a stream of raw packfile parse output.
+// It first parses the stream and saves all the objects to disk.
+// Along the way it records in memory deltas that need applying
+// Then using recursive sweeps it converts all the deltas to real objects.
+// Returns a continuable with a data report.
+module.exports = hydrate;
+function hydrate(stream, db, callback) {
+  if (!callback) return hydrate.bind(this, stream, db);
 
-  // Nest the bodies
-  stream = subStream(stream, function (item) {
-    return !bops.is(item);
-  }, "body");
+  var version;
+  var num;
+  var count = 0;
+  var deltas = 0;
 
-  var reading = false;
-  var dataQueue = [];
-  var readQueue = [];
-  // Number of pending delta objects waiting on their target to be found.
-  var pending = 0;
-  // offset to hash mapping
+  // lists of deltas waiting by target hash
+  var waiting = {};
+
+  // map from offset to hash
+  var offsets = {};
+  // hashes of full ready objects
   var hashes = {};
+  // queue of target hashes who's dependendents need to be resolved.
+  var queue = [];
 
-  return { read: read, abort: stream.abort };
+  stream.read(onHeader);
 
-  function read(callback) {
-    readQueue.push(callback);
-    check();
-  }
-
-  function check() {
-    // Flush the queues.
-    while (dataQueue.length && readQueue.length) {
-      readQueue.shift().apply(null, dataQueue.shift());
-    }
-    // If we want for data, try to get more.
-    if (!reading && readQueue.length) {
-      reading = true;
-      stream.read(onRead);
-    }
-  }
-
-  function onRead(err, item) {
-    reading = false;
-
-    // Check for leftover deltas when forwarding end of stream information.
-    if (item === undefined) {
-      if (!err && pending) {
-        err = new Error(pending + " delta targets were never found before end");
-      }
-      dataQueue.push([err]);
-    }
-
-    // If the item is a delta, try to resolve it.
-    else if (item && item.ref) {
-      // Convert offset refs into normal hash based refs
-      if (typeof item.ref === "number") {
-        var hash = hashes[item.offset - item.ref];
-        if (!hash) throw new Error("Cann't find back-reference");
-        item.ref = hash;
-      }
-      pending++;
-      find(item, onFind);
-    }
-
-    // Otherwise, forward all other traffic through.
-    else {
-      item.type = types[item.type];
-      tap(item);
-      dataQueue.push([null, item]);
-    }
-    check();
-  }
-
-  // Item here may or may not be the same object as was passed into find
-  // since it may have been put in offline storage temporarily.
-  function onFind(err, patch, base) {
-    function callback() {
-      dataQueue.push(arguments);
-      check();
-    }
+  function onHeader(err, info) {
     if (err) return callback(err);
-    console.log(patch.ref, "FOUND");
-    pending--;
-    applyDelta(patch.body, base.body)(function (err, output) {
+    version = info.version;
+    num = info.num;
+    // Convert the stream have nested bodies
+    stream = subStream(stream, function (item) {
+      return !bops.is(item);
+    }, "body");
 
-      if (output === undefined) return callback(err);
-      if (output.baseLen !== base.length) {
-        return callback(new Error("Base length mismatch " + output.baseLen + " != " + base.length));
+    each(stream, onObject)(onReceived);
+  }
+
+  function progress() {
+    var percent = Math.round(count / num * 100);
+    return "Receiving objects:  " + percent + "% (" + count + "/" + num + ")";
+  }
+
+  function onObject(obj) {
+    process.stdout.write(progress() + "\r");
+    db.save(obj, function (err, hash) {
+      if (err) return callback(err);
+      offsets[obj.offset] = hash;
+      if (obj.ref) {
+        deltas++;
+        if (typeof obj.ref === "number") {
+          obj.ref = offsets[obj.offset - obj.ref];
+        }
+        if (waiting[obj.ref]) waiting[obj.ref].push(hash);
+        else waiting[obj.ref] = [hash];
       }
-      var item = tap({
-        offset: patch.offset,
-        type: base.type,
-        length: output.targetLen,
-        body: { read: output.read, abort: output.abort },
-        merged: true
-      });
-      callback(null, item);
+      else {
+        hashes[hash] = true;
+      }
+      count++;
     });
   }
 
-  // Tap an object's substream calculating the sha1sum along the way
-  // When done, store hash on item and save the offset -> hash mapping.
-  function tap(item) {
-    var stream = item.body;
-    var bytes = 0;
+  function onReceived(err) {
+    if (err) return callback(err);
+    process.stdout.write(progress() + "\n");
+    Object.keys(waiting).forEach(function (hash) {
+      if (hashes[hash]) queue.push(hash);
+    });
+    offsets = null;
+    hashes = null;
+    count = 0;
 
-    var sha1sum = sha1();
-    sha1sum.update(item.type + " " + item.length + "\0");
+    console.log({
+      waiting: waiting,
+      queue: queue
+    });
 
-    item.body = { read: tappedRead, abort: stream.abort };
-    item.hash = null;
-
-    return item;
-
-    function tappedRead(callback) {
-      stream.read(function (err, chunk) {
-        if (err) return callback(err);
-        if (chunk === undefined) {
-          var hash = sha1sum.digest();
-          if (item.hash && item.hash !== hash) {
-            return callback(new Error("Sha1sum mismatch: " + item.hash + " != " + hash));
-          }
-          hashes[item.offset] = item.hash = hash;
-          if (bytes !== item.length) {
-            return callback(new Error("Length mismatch:  Expected " + item.length + ", but was " + bytes));
-          }
-          return callback();
-        }
-        bytes += chunk.length;
-        // console.log(chunk.toString());
-        sha1sum.update(chunk);
-        callback(null, chunk);
-      });
-    }
+    each(arraySource(queue), onJob)(onApplied);
   }
 
-};
+  function onJob(job) {
+    console.log("job", job);
 
+  }
+
+  function onApplied(err) {
+    if (err) return callback(err);
+    console.log("DONE?");
+  }
+}
+
+//   var input =
+// module.exports = function (stream, db) {
+//
+//   // Nest the bodies
+//   stream = subStream(stream, function (item) {
+//     return !bops.is(item);
+//   }, "body");
+//
+//   var reading = false;
+//   var dataQueue = [];
+//   var readQueue = [];
+//   // Number of pending delta objects waiting on their target to be found.
+//   var pending = 0;
+//   // offset to hash mapping
+//   var hashes = {};
+//   var seen = {};
+//
+//   return { read: read, abort: stream.abort };
+//
+//   function read(callback) {
+//     readQueue.push(callback);
+//     check();
+//   }
+//
+//   function check() {
+//     // Flush the queues.
+//     while (dataQueue.length && readQueue.length) {
+//       readQueue.shift().apply(null, dataQueue.shift());
+//     }
+//     // If we want for data, try to get more.
+//     if (!reading && readQueue.length) {
+//       reading = true;
+//       stream.read(onRead);
+//     }
+//   }
+//
+//   function onRead(err, item) {
+//     reading = false;
+//
+//     // Check for leftover deltas when forwarding end of stream information.
+//     if (item === undefined) {
+//       if (!err && pending) {
+//         err = new Error(pending + " delta targets were never found before end");
+//       }
+//       dataQueue.push([err]);
+//       return check();
+//     }
+//
+//     seen[item.offset] = item;
+//
+//     // If the item is a delta, try to resolve it.
+//     if (item && item.ref) {
+//       // Convert offset refs into normal hash based refs
+//       if (typeof item.ref === "number") {
+//         var target = item.offset - item.ref;
+//         var hash = hashes[target];
+//         if (!hash) {
+//           if (!seen[target]) {
+//             var keys = Object.keys(hashes).filter(function (key) {
+//               key = parseInt(key, 10);
+//               return target - 1000 < key && target + 1000 > key;
+//             });
+//             console.log("near keys", keys);
+//             throw new Error("Can't find back-reference");
+//           }
+//           item.target = seen[target];
+//           console.log("WARNING: sending ref instead of hash", item)
+//         }
+//         else {
+//           item.ref = hash;
+//         }
+//       }
+//       pending++;
+//       find(item, onFind);
+//     }
+//
+//     // Otherwise, forward all other traffic through.
+//     else {
+//       item.type = types[item.type];
+//     tap(item);
+//       dataQueue.push([null, item]);
+//     }
+//     check();
+//   }
+//
+//   // Item here may or may not be the same object as was passed into find
+//   // since it may have been put in offline storage temporarily.
+//   function onFind(err, patch, base) {
+//     function callback() {
+//       dataQueue.push(arguments);
+//       check();
+//     }
+//     if (err) return callback(err);
+//     console.log(patch.ref, "FOUND");
+//     pending--;
+//     applyDelta(patch.body, base.body)(function (err, output) {
+//
+//     });
+//   }
+//
+//   function onApplied(err, output) {
+//     if (output === undefined) return callback(err);
+//     if (output.baseLen !== base.length) {
+//       return callback(new Error("Base length mismatch " + output.baseLen + " != " + base.length));
+//     }
+//     var item = tap({
+//       offset: patch.offset,
+//       type: base.type,
+//       length: output.targetLen,
+//       body: { read: output.read, abort: output.abort },
+//       merged: true
+//     });
+//     callback(null, item);
+//   }
+//
+//   // Tap an object's substream calculating the sha1sum along the way
+//   // When done, store hash on item and save the offset -> hash mapping.
+//   function tap(item) {
+//     var stream = item.body;
+//     var bytes = 0;
+//
+//     var sha1sum = sha1();
+//     sha1sum.update(item.type + " " + item.length + "\0");
+//
+//     item.body = { read: tappedRead, abort: stream.abort };
+//     item.hash = null;
+//
+//     return item;
+//
+//     function tappedRead(callback) {
+//       stream.read(function (err, chunk) {
+//         if (err) return callback(err);
+//         if (chunk === undefined) {
+//           var hash = sha1sum.digest();
+//           if (item.hash && item.hash !== hash) {
+//             return callback(new Error("Sha1sum mismatch: " + item.hash + " != " + hash));
+//           }
+//           hashes[item.offset] = item.hash = hash;
+//           if (bytes !== item.length) {
+//             return callback(new Error("Length mismatch:  Expected " + item.length + ", but was " + bytes));
+//           }
+//           return callback();
+//         }
+//         bytes += chunk.length;
+//         // console.log(chunk.toString());
+//         sha1sum.update(chunk);
+//         callback(null, chunk);
+//       });
+//     }
+//   }
+//
+// };
+//
+//
+//
+//
+// var targets = {};
+// var targetIndex = {};
+//
+// function addDep(hash, target) {
+//   if (target in targetIndex) {
+//
+//   }
+//   else {
+//
+//   }
+//
+// }
--- test/test-hydrate.js
+++ test/test-hydrate.js
@@ -1,93 +1,15 @@
 var pushToPull = require('push-to-pull');
-var parse = pushToPull(require('../parse.js'));
-var hydrate = require('../hydrate.js');
 var readStream = require('./fs.js').readStream;
-var binarySource = require('simple-stream-helpers/binary-source.js');
-var each = require('simple-stream-helpers/each.js');
-var consume = require('simple-stream-helpers/consume.js');
-var slow = require('simple-stream-helpers/slow.js');
-var bops = require('bops');
+var parse = pushToPull(require('../parse.js'));
+var db = require('./db.js')();
+var hydrate = require('../hydrate.js');
 
 // Stream is raw pack file as seen on disk
 var stream = readStream(process.argv[2] || __dirname + "/sample.pack");
 
-// stream is now flat stream of interpolated headers and bodies
-stream = parse(stream, false);
-
-// Stream is now nested streams with fully hydrated bodies.
-stream = hydrate(stream, find);
-
-// objects and bodies are cached in ram for easy retrieval
-var objects = {};
-
-// Pending finds.  Key is hash of target, value is array of patches with callbacks.
-var pending = {};
-
-each(stream, function (object) {
-  store(object, function (err, copy) {
-    if (err) throw err;
-    objects[copy.hash] = copy;
-    flush(copy);
-    console.log(object.hash, object.type, object.length, object.merged ? "merged": "");
-  });
-})(function (err) {
-  console.log(pending);
+hydrate(parse(stream), db)(function (err, report) {
+  console.log(arguments);
+  console.log((new Error).stack);
   if (err) throw err;
-  console.log("END");
+  console.log("report", report);
 });
-
-function find(item, callback) {
-  var hash = item.ref;
-  console.log(hash, "SEARCHING...");
-
-  // If the target is here, send it out.
-  var cached = objects[hash];
-  if (cached) {
-    return callback(null, item, load(cached));
-  }
-
-  // Store the callback and delta object for later
-  store(item, function (err, copy) {
-    if (err) return callback(err);
-    var action = { callback: callback, copy: copy };
-    if (!pending[hash]) pending[hash] = [action];
-    else pending[hash].push(action);
-  });
-}
-
-// Flush any pending finds
-function flush(cached) {
-  var actions = pending[cached.hash];
-  if (!actions) return;
-  delete pending[cached.hash];
-  actions.forEach(function (action) {
-    action.callback(null, load(action.copy), load(cached));
-  });
-}
-
-// Store an object in memory with it's body buffered as an array.
-function store(object, callback) {
-  var copy = {};
-  for (var key in object) {
-    if (key === "body") continue;
-    copy[key] = object[key];
-  }
-  consume(object.body)(function (err, items) {
-    if (err) return callback(err);
-    copy.hash = object.hash;
-    copy.body = bops.join(items);
-    callback(null, copy);
-  });
-}
-
-// Convert a saved object to a live streamable object.
-function load(copy) {
-  var object = {};
-  for (var key in copy) {
-    if (key === "body") continue;
-    object[key] = copy[key];
-  }
-  object.body = binarySource(copy.body, 0x100);
-
-  return object;
-}
