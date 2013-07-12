0a4e07368cfc39a8ffb9b529f73ec3bf955ab730
Tim Caswell <tim@creationix.com>
2013-07-12 15:35:07 -0500

    Allow easy switching backends

--- test/test-hydrate.js
+++ test/test-hydrate.js
@@ -2,9 +2,17 @@ var pushToPull = require('push-to-pull');
 var readStream = require('./fs.js').readStream;
 var parse = pushToPull(require('../parse.js'));
 var hydrate = require('../hydrate.js');
-var fs = require('simple-fs')(__dirname + "/test.git");
-require('git-fs-db')(fs, { bare: true, init: true }, function (err, db) {
-  if (err) throw err;
+
+test(require('./db.js')());
+
+// Uncomment to use a real fs backed db.
+// var fs = require('simple-fs')(__dirname + "/test.git");
+// require('git-fs-db')(fs, { bare: true, init: true }, function (err, db) {
+//   if (err) throw err;
+//   test(db);
+// });
+
+function test(db) {
   // Stream is raw pack file as seen on disk
   var stream = readStream(process.argv[2] || __dirname + "/sample.pack");
 
@@ -13,6 +21,6 @@ require('git-fs-db')(fs, { bare: true, init: true }, function (err, db) {
     console.log("report", report);
   });
 
-});
+}
 
 
