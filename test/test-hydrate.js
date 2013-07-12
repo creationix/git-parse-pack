547e3d6045b8353bde20dfab8435edd2b05e8061
Tim Caswell <tim@creationix.com>
2013-07-12 09:33:44 -0500

    Use real git repo for test

--- test/test-hydrate.js
+++ test/test-hydrate.js
@@ -1,15 +1,18 @@
 var pushToPull = require('push-to-pull');
 var readStream = require('./fs.js').readStream;
 var parse = pushToPull(require('../parse.js'));
-var db = require('./db.js')();
 var hydrate = require('../hydrate.js');
-
-// Stream is raw pack file as seen on disk
-var stream = readStream(process.argv[2] || __dirname + "/sample.pack");
-
-hydrate(parse(stream), db)(function (err, report) {
-  console.log(arguments);
-  console.log((new Error).stack);
+var fs = require('simple-fs')(__dirname + "/test.git");
+require('git-fs-db')(fs, { bare: true, init: true }, function (err, db) {
   if (err) throw err;
-  console.log("report", report);
+  // Stream is raw pack file as seen on disk
+  var stream = readStream(process.argv[2] || __dirname + "/sample.pack");
+
+  hydrate(parse(stream), db)(function (err, report) {
+    if (err) throw err;
+    console.log("report", report);
+  });
+
 });
+
+
